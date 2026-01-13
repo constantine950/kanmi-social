eba44d4dfefcfa91df7f6f0ffe86e77401b0146e
james <omotoyosiadebeshin@gmail.com>
2026-01-13 23:57:53 +0100

    cors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,32 +11,17 @@ import AppLayout from "./components/AppLayout";
 import Toast from "./components/Toast";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { useEffect } from "react";
-import { refreshToken } from "./api/authApi";
 import { disconnectSocket, initSocket } from "./socket";
 import { useAuthStore } from "./zustand/authStore";
 
 function App() {
-  const setAuth = useAuthStore((s) => s.setAuth);
   const user = useAuthStore((s) => s.user);
   const setAuthLoading = useAuthStore((s) => s.setAuthLoading);
-  const clearAuth = useAuthStore((s) => s.clearAuth);
 
+  // Just stop the loading spinner immediately
   useEffect(() => {
-    const restoreSession = async () => {
-      setAuthLoading(true);
-
-      try {
-        const res = await refreshToken();
-        setAuth(res.data.user, res.data.newAccessToken);
-      } catch {
-        clearAuth();
-      } finally {
-        setAuthLoading(false);
-      }
-    };
-
-    restoreSession();
-  }, [setAuth, setAuthLoading, clearAuth]);
+    setAuthLoading(false);
+  }, [setAuthLoading]);
 
   useEffect(() => {
     if (user) {
